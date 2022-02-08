49ac96d2b7e958f5a3bea2b252cfa2d25459abd0
ymcla <ymclapp@yahoo.com>
2022-02-08 11:55:18 -0600

    added productcard and css and updated homescreen to show products

---
+++ src/components/ProductCard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ProductCard = ({product}) => {
+  return <div className='productCard__wrapper'>
+      <img className = 'productCard__img' src = {product.image}
+      alt=''
+      />
+      <h4>{product.name}</h4>
+      <div className='productCard__price'>
+          <h5>${product.price}</h5>
+          </div>
+  </div>;
+}
+
+export default ProductCard;
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,17 +2,18 @@ import React from 'react';
 import './HomeScreen.css';
 
 import products from '../data';
+import ProductCard from '../components/ProductCard';
 
 
 
 const HomeScreen = () => {
     return (
         <div>
-{products.map((product) => (
-    product.name
-))}
+            {products.map((product) => (
+                <ProductCard key={product.id} product={product} />
+            ))}
         </div>
-    )
-}
+    );
+};
 
 export default HomeScreen;
