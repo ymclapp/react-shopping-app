5fd8be412472d9843984728a37050f6c00e1a0a8
ymcla <ymclapp@yahoo.com>
2022-02-08 14:01:51 -0600

    started to use the format-currency, but it gave an assert error

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,8 +1,10 @@
 import React from 'react';
+//import formatCurrency from 'format-currency';
 
 import './ProductCard.css';
 
 const ProductCard = ({product}) => {
+    //let opts = { format: '%s%v', symbol: '$' };  //using format-currency had an assert error
   return <div className='productCard__wrapper'>
       <img className = 'productCard__img' src = {product.image}
       alt=''
@@ -11,6 +13,7 @@ const ProductCard = ({product}) => {
       <div className='productCard__price'>
           <h5>${product.price}</h5>
           </div>
+          <button className = 'ProductCard__button'>Add to Cart</button>
   </div>;
 }
 
