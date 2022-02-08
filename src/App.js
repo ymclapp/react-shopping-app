0ed55c7c71a1150ec6d54a6eab80141adecd26d0
ymcla <ymclapp@yahoo.com>
2022-02-08 15:51:07 -0600

    created the card and its css and added to app.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 
 import Nav from './components/Nav';
+import Cart from './components/Cart';
 import HomeScreen from './screens/HomeScreen';
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <Cart />
       <HomeScreen />
     </div>
   );
